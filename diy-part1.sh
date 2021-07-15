616f5b88f1c65c83a2d408aac87fc66a860e04bc
Qunter-2233 <62251316+Qunter-2233@users.noreply.github.com>
2021-07-15 21:23:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,5 +25,5 @@ git clone https://github.com/kenzok8/small.git package/small
 #wget https://github.com/upx/upx/releases/download/v3.95/upx-3.95-amd64_linux.tar.xz
 #tar -Jxf upx*.tar.xz
 #cp upx*/upx /usr/bin
-#cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin
-#cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin
+sudo cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin
+sudo cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin
