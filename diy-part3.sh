1cf7107c73c29e9ce02ee77b8f11c1c6cc0c6771
Qunter-2233 <62251316+Qunter-2233@users.noreply.github.com>
2021-07-16 08:07:57 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -16,14 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
-#git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
-#git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
-#git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#wget https://github.com/upx/upx/releases/download/v3.95/upx-3.95-amd64_linux.tar.xz
-#tar -Jxf upx*.tar.xz
-#cp upx*/upx /usr/bin
-cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin
-cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin
